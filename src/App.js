4b0b28379d10047cbd95aa60505a81f20b4261d1
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-15 21:04:52 -0300

    github pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MovieCard from "./MovieCard";
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=861a176';
+const API_URL = 'https://www.omdbapi.com/?apikey=861a176';
 
 const App = () => {
 
